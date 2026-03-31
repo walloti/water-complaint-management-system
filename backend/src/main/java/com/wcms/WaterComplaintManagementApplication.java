f2e5fe96cf1561cdd8055b80c6d518f58952ba0f
Wallace Oduor Otieno <wallaceotieno4@gmail.com>
2026-03-31 20:47:33 +0300

    Add main Spring Boot application class WaterComplaintManagementApplication.java

---
+++ backend/src/main/java/com/wcms/WaterComplaintManagementApplication.java
@@ -0,0 +1,18 @@
+package com.wcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class WaterComplaintManagementApplication implements WebMvcConfigurer {
+    public static void main(String[] args) {
+        SpringApplication.run(WaterComplaintManagementApplication.class, args);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+    }
+}
